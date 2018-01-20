ce3b844ab4397fd34b58a6a8dae87355a9500538
ssgroups2187 <santhanakumar.niet@gmail.com>
2018-01-20 18:04:26 +0530

    Updated banner bg

--- js/main.js
+++ js/main.js
@@ -10,10 +10,16 @@ $(function(){
             $('.error').show().text(e);
         }
     }
-    /* list_screen_slide Active
-    =============================*/
-    /* single_screen_slide Active
-    =============================*/
+    // Automatic drops
+	setInterval(function() {
+		var $el = $('.ripple');
+		var x = Math.random() * $el.outerWidth();
+		var y = Math.random() * $el.outerHeight();
+		var dropRadius = 20;
+		var strength = 0.04 + Math.random() * 0.04;
+
+		$el.ripples('drop', x, y, dropRadius, strength);
+	}, 400);
     var single_screen_slide = $('.single_screen_slide');
     single_screen_slide.owlCarousel({
         loop: true,
@@ -41,15 +47,15 @@ $(function(){
         center: true,
         items: 1,
     });
-    // Automatic drops
-	setInterval(function() {
-		var $el = $('.ripple');
-		var x = Math.random() * $el.outerWidth();
-		var y = Math.random() * $el.outerHeight();
-		var dropRadius = 20;
-		var strength = 0.04 + Math.random() * 0.04;
-
-		$el.ripples('drop', x, y, dropRadius, strength);
-	}, 400);
+    $('.video-popup').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup: '<style>.mfp-iframe-holder .mfp-content {max-width: 900px;height:500px}</style>' +
+                '<div class="mfp-iframe-scaler" >' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div></div>'
+        }
+    });
 
 });
